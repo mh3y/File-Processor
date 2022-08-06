98aed756b151a827c3f56b89af2e9a6e7a84c477
Michael Yang <michaelyangz49@gmail.com>
2022-08-06 23:59:22 +1000

    Testing for existence of a key in dictionary

--- backup_tester.py
+++ backup_tester.py
@@ -45,28 +45,34 @@ def main():
     keywords_json = json.load(keywords_file)  # converting JSON file into an object
     keywords_file.close()  # closing the file
 
-    sql_files_json = []  # creating a list
-    for file_path in getlistofsqlfiles(dirName='//Users//mh3y//Documents//vscode//SQL_Processor//SQL_Scripts'):
-        list_of_words_found_for_this_file = []  # creating a list
-        for words in keywords_json:  # for loop to iterate through keywords_json for "words"
-            with open(file_path) as file:
-                contents = file.read()
-                search_word = words["Keyword"]
-                if file_path == words["whitelist"]:
-                    continue
-                else:
-                    if search_word in contents:
-                        list_of_words_found_for_this_file.append(search_word)
-        if (len(list_of_words_found_for_this_file)) > 0:
-            # print(file_path)
-            # print(list_of_words_found_for_this_file)
-            # print(len(list_of_words_found_for_this_file))
-            sql_filename = os.path.split(file_path)[1]
-            sql_parent_path = os.path.split(os.path.split(file_path)[0])[1]
-            sql_files_json.append({'filename': sql_filename, 'parent_path': sql_parent_path,
-                                   'fullpath': file_path, 'keyword': list_of_words_found_for_this_file})
-
-    print(sql_files_json)
+    # sql_files_json = []  # creating a list
+    #for file_path in getlistofsqlfiles(dirName='//Users//mh3y//Documents//vscode//SQL_Processor//SQL_Scripts'):
+    list_of_words_found_for_this_file = []  # creating a list
+    for words in keywords_json:  # for loop to iterate through keywords_json for "words"
+        #  print(words)
+        # with open(file_path) as file:
+        #     contents = file.read()
+        #     search_word = words["Keyword"]
+        if "whitelist" in words:
+            print("whitelist exists")
+        else:
+            continue
+        #
+        #     if file_path == words["whitelist"]:
+        #         continue
+        #     else:
+        #         if search_word in contents:
+        #             list_of_words_found_for_this_file.append(search_word)
+#     if (len(list_of_words_found_for_this_file)) > 0:
+#         # print(file_path)
+#         # print(list_of_words_found_for_this_file)
+#         # print(len(list_of_words_found_for_this_file))
+#         sql_filename = os.path.split(file_path)[1]
+#         sql_parent_path = os.path.split(os.path.split(file_path)[0])[1]
+#         sql_files_json.append({'filename': sql_filename, 'parent_path': sql_parent_path,
+#                                'fullpath': file_path, 'keyword': list_of_words_found_for_this_file})
+#
+# print(sql_files_json)
 
 
 if __name__ == '__main__':

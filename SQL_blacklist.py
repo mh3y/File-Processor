04562fdcab9b582ab0be79d327f632f26427a41f
Michael Yang <michaelyangz49@gmail.com>
2022-08-20 20:10:53 +1000

    Update

--- SQL_blacklist.py
+++ SQL_blacklist.py
@@ -94,7 +94,7 @@ def change_to_txt(sql_files_json):
 def get_github_user(user_name):
     url = 'https://api.github.com'
     url_users = '/users/'
-    h = {'Accept': 'application/vnd.github+json', 'Authorization': 'token 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'}
+    h = {'Accept': 'application/vnd.github+json', 'Authorization': 'token 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'}
     print('TODO: call {}{}{}'.format(url, url_users, user_name))
     r = requests.get(url + url_users + user_name, headers=h)
     print(r.json())
@@ -103,55 +103,68 @@ def get_github_user(user_name):
         print('Sadly the user {} does not exist, but may be a thousand years from now, he will be.'.format(user_name))
 
 
-def create_github_issue(user_name):
+def create_github_issue(user_name, new_issue):
     url = 'https://api.github.com'
-    url_repos = '/repos/{}/File-Processor/issues'.format(user_name)
-    h = {'Accept': 'application/vnd.github+json', 'Authorization': 'token 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'}
-    new_issue = {
-        'owner': user_name,
-        'repo': "File-Processor",
-        'title': "bougie-01",
+    repo_name = 'File-Processor'
+    url_repos = '/repos/{}/{}/issues'.format(user_name, repo_name)
+    h = {'Accept': 'application/vnd.github+json', 'Authorization': 'token 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'}
+    # print(new_issue)
+    # print('TODO: call {}{}{}'.format(url, url_repos, user_name))
+    if is_github_issue_title_exists(new_issue['owner'], new_issue['repo'], new_issue['title']):
+        return
+    else:
+        r = requests.post(url + url_repos, headers=h, data=json.dumps(new_issue))
+    # print(r.json())
+    # print('Status Code returned:{}'.format(r.status_code))
+    if r.status_code != 201:
+        print('error - status code is {}'.format(r.status_code))
+
+
+def get_github_issues(user_name, repo_name):
+    url = 'https://api.github.com'
+    url_repos = '/repos/{}/{}/issues'.format(user_name, repo_name)
+    h = {'Accept': 'application/vnd.github+json', 'Authorization': 'token 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'}
+    r = requests.get(url + url_repos, headers=h)
+    return r.json()
+
+
+def list_github_issues(user_name, repo_name):
+    issues = get_github_issues(user_name=user_name, repo_name=repo_name)
+    for issue in issues:
+        print('{}, {}, {}, {}'.format(issue['id'], issue['title'], issue['state'], issue['created_at']))
+
+
+def is_github_issue_title_exists(user_name, repo_name, issue_title):
+    issues = get_github_issues(user_name=user_name, repo_name=repo_name)
+    filtered_issues = [issue for issue in issues if issue['title'] == issue_title]
+    count_of_found_issues = len(filtered_issues)
+    if count_of_found_issues > 0:
+        return True
+    else:
+        return False
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rename", action='store_true')
+    parser.add_argument("--print", action='store_true')
+    parser.add_argument("--gituser", required=False)
+    parser.add_argument("--postissue", action='store_true', required=False)
+    parser.add_argument('--listissues', required=False, action='store_true')
+    parser.add_argument("--isissueexists", type=bool, required=False)
+    parser.add_argument("--dirName", type=str, required=True)
+    args = parser.parse_args()
+
+    github_repo_name = 'File-Processor'
+    test_issue = {
+        'owner': args.gituser,
+        'repo': github_repo_name,
+        'title': "bougie-02",
         'body': "smells like patchouli",
         'assignees': [],
         'milestone': None,
         'labels': []
     }
-    # print(new_issue)
-    # print('TODO: call {}{}{}'.format(url, url_repos, user_name))
-    r = requests.post(url + url_repos, headers=h, data=json.dumps(new_issue))
-    # print(r.json())
-    # print('Status Code returned:{}'.format(r.status_code))
-    if r.status_code != 201:
-        print('error - status code is {}'.format(r.status_code))
-        # print('Sadly the user {} does not exist, but may be a thousand years from now, he will be.'.format(user_name))
-
-    # print('TODO: create a github issue for each blacklisted file for {}'.format(user_name))
-
-
-def list_github_issues(user_name, repo_name):
-    url = 'https://api.github.com'
-    url_repos = '/repos/{}/{}/issues'.format(user_name, repo_name)
-    h = {'Accept': 'application/vnd.github+json', 'Authorization': 'token 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'}
-    r = requests.get(url + url_repos, headers=h)
-    for i in r.json():
-        print(i["title"])
-
-
-
-
-def main():
-    github_repo_name = 'File-Processor'
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--rename", action='store_true')
-    parser.add_argument("--print", action='store_true')
-    parser.add_argument("--gituser", required=False)
-    parser.add_argument("--postissue", action='store_false', required=False)
-    parser.add_argument('--listissues', required=False, action='store_false')
-    parser.add_argument("--dirName", type=str, required=True)
-    args = parser.parse_args()
-
-    list_github_issues(user_name=args.gituser, repo_name=github_repo_name)
 
     sql_files_json = blacklist_processor(args.dirName)
 
@@ -163,9 +176,11 @@ def main():
     # if args.gituser:
     #     get_github_user(args.gituser)
     if args.postissue is True:
-        create_github_issue(args.gituser)
+        create_github_issue(args.gituser, test_issue)
     if args.listissues is True:
-        list_github_issues(args.gituser)
+        list_github_issues(user_name=args.gituser, repo_name=github_repo_name)
+    if args.isissueexists is True:
+        print(args.isissueexists)
 
 
 if __name__ == '__main__':
